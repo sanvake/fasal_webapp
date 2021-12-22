dfeb7850d4588af3eb4b902f3bb8e2cc3b596bb9
Vake <not.vakesan@gmail.com>
2021-12-22 23:02:23 +0530

    beautified signup

--- fasal-frontend/src/pages/Signup.js
+++ fasal-frontend/src/pages/Signup.js
@@ -7,6 +7,7 @@ const Signup = () => {
     const [form, setform] = useState({name:'',email:'',password:'',confirmPassword:"",age:""});
     const handleSubmitSignup = async (e) => {
         e.preventDefault();
+        if(!form.name || !form.email || !form.password || !form.confirmPassword || !form.age){ return alert("Something is Empty, Please check your wallet.")};
         if(form?.password!==form?.confirmPassword){ return alert("Passord not matching")};
         try {
             var {data} = await signup(form.name, form.email, form.password, form.age);
@@ -25,13 +26,21 @@ const Signup = () => {
     }
 
     return (
-        <div>
-                <span>name : <input type="text" placeholder='Your Name' onChange={(e)=>(setform({...form, name: e.target.value}))} /></span><br/>
-                <input type="email" placeholder='Your email' onChange={(e)=>(setform({...form, email: e.target.value}))} /><br/>
-                <input type="password" placeholder='Password' onChange={(e)=>(setform({...form, password: e.target.value }))} /><br/>
-                <input type="password" placeholder='Confirm password' onChange={(e)=>(setform({...form, confirmPassword : e.target.value}))} /><br/>
-                <input type="number" placeholder='Age' onChange={(e)=>(setform({...form, age: e.target.value}))} /><br/>
+        <div style={{display:'flex',justifyContent:'center',alignItems:'center',width:'100%',height:'50vh', flexDirection:'column'}}>
+            <h2>Sign Up</h2>
+            <div>
+                <label for="name">Name :</label><br/>
+                <input id="name" type="text" placeholder='Your Name' onChange={(e)=>(setform({...form, name: e.target.value}))} /><br/>
+                <label for="age">Age :</label><br/>
+                <input id="age" type="number" placeholder='Age' onChange={(e)=>(setform({...form, age: e.target.value}))} /><br/>
+                <label for="email">Email :</label><br/>
+                <input id="email" type="email" placeholder='Your email' onChange={(e)=>(setform({...form, email: e.target.value}))} /><br/>
+                <label for="pass">Password :</label><br/>
+                <input id="pass" type="password" placeholder='Password' onChange={(e)=>(setform({...form, password: e.target.value }))} /><br/>
+                <label for="cpass">Confirm Password :</label><br/>
+                <input id="cpass" type="password" placeholder='Confirm password' onChange={(e)=>(setform({...form, confirmPassword : e.target.value}))} /><br/><br/>
                 <button onClick={handleSubmitSignup} >Submit</button>
+            </div>
         </div>
     )
 }
